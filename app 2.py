780ec306dbb5dbdba6815ece406d985980a6b306
Sasha755-ops <asanov_sasha@icloud.com>
2025-04-08 21:43:33 +0200

    Add files via upload

---
+++ app 2.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+UPLOAD_FOLDER = 'static/uploads'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(url_for('index'))
+    if file:
+        filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
